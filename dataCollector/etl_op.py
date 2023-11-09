722204232d1893bb5758c32260763c3818260ced
Kuo-Shou Tsai <kstsai@atayalan.com>
2023-11-09 16:49:03 +0800

    change gnb-dp.png output column

--- dataCollector/etl_op.py
+++ dataCollector/etl_op.py
@@ -287,7 +287,7 @@ def visualize_gNB_usage(dfGnbUsage,outfn="/tmp/gNB-usage.png"):
     """
     dfHourly,bgn,end = transform_to_hourly(dfGnbUsage)
     df1 = dfHourly[["utc_timestamp_min","dlMBps", "ulMBps"]]
-    df2 = dfHourly[["utc_timestamp_min","dlPKTps", "ulPKTps"]]
+    df2 = dfHourly[["utc_timestamp_min","gnbCnt", "upTunnelCnt"]]
 
     figTitle = f"{bgn.strftime('%b %d')} => {end.strftime('%b %d')} ({end-bgn})"
 
@@ -296,7 +296,7 @@ def visualize_gNB_usage(dfGnbUsage,outfn="/tmp/gNB-usage.png"):
 
     #add DataFrames to subplots
     df1.plot(ax=axes[0],x="utc_timestamp_min",title=figTitle)
-    df2.plot(ax=axes[1],x="utc_timestamp_min")
+    df2.plot(ax=axes[1],x="utc_timestamp_min",kind="bar")
     plt.savefig(outfn)
 
 def visualize_dp_usage(dfDpUsage,vmDut,outfn="/tmp/DP-usage.png"):
